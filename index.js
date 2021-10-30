169d62b5aa01dce1e5bd706d8a68b3202a1fbd63
Le Duong Anh Chi <17521293@gm.uit.edu.vn>
2021-10-30 23:08:07 +0700

    third

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 8080
+const PORT = process.env.PORT || 8080;
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.send('cuong cuoi ')
   })
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
